097e6a1f0ee1698577c95a577a9ee8791c83cd27
jayanagavenkatasudhakar <jaya.sudhakar22@gmail.com>
2018-11-29 16:11:58 +0530

    Added sample java pgm

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        System.out.println("Hello !!!1 jaya hru & Date:22nd nov???"); 
+		System.out.println("Good Mr.Jaya");
+    } 
+} 
